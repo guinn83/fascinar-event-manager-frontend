5fd48e60361976cffb423ea52f04586bb95496c8
Wagner Alves <guinn83@hotmail.com>
2022-11-02 19:37:56 -0300

    Fix Eventlist center view

--- src/pages/Events/index.tsx
+++ src/pages/Events/index.tsx
@@ -5,7 +5,7 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import { Box, Container, Grid, IconButton, ListItemIcon, Paper } from '@mui/material';
+import { Box, Container, IconButton, ListItemIcon, Paper } from '@mui/material';
 import NavBar from '../../components/AppBar';
 import axios from 'axios';
 import authHeader from '../../services/auth-header';
@@ -14,8 +14,8 @@ import { EventModel } from '../../models/eventModel';
 import { Fragment, useEffect, useState } from 'react';
 import "./styles.css";
 import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
-import EventPage from '../EventPage';
 import { Link } from 'react-router-dom';
+import { display } from '@mui/system';
 
 const API_URL = `${BASE_URL}`;
 
@@ -37,75 +37,77 @@ export default function EventList() {
     }, [refreshKey]);
 
     return (
-        <Box className='container' >
-            <NavBar />    
-            <List  sx={{ width: '100%', maxWidth: '100%', bgcolor: 'background.paper' }}>
-                {
-                    eventmodel.map(event => {
+        <Container className='container' maxWidth={false}  disableGutters={true}  >
+            <NavBar />
+            <Box className='content'>
+                <List className="event-list">
+                    {
+                        eventmodel.map(event => {
 
-                        let dateEvent = new Date(event.eventDate);
-                        let status;
+                            let dateEvent = new Date(event.eventDate);
+                            let status;
 
-                        return (
-                            <Paper className='el-paper' elevation={3} >
-                                <Link to={'/event/' + event.id} style={{ textDecoration: 'none' }}>
-                                    <Typography className='typ-status' event-status={event.status}>
-                                        {status}
-                                    </Typography>
+                            return (
+                                <Paper className='el-paper' elevation={3} >
+                                    <Link to={'/event/' + event.id} style={{ textDecoration: 'none' }}>
+                                        <Typography className='typ-status' event-status={event.status}>
+                                            {status}
+                                        </Typography>
 
-                                    <ListItem alignItems='flex-start'  >
-                                        <ListItemAvatar>
-                                            <Avatar
-                                                className='avatar-list'
-                                                alt=""
-                                                src="/static/images/avatar/1.jpg"
-                                            //variant="rounded"
-                                            />
-                                        </ListItemAvatar>
+                                        <ListItem alignItems='flex-start'  >
+                                            <ListItemAvatar>
+                                                <Avatar
+                                                    className='avatar-list'
+                                                    alt=""
+                                                    src="/static/images/avatar/1.jpg"
+                                                    //variant="rounded"
+                                                />
+                                            </ListItemAvatar>
 
-                                        <ListItemText
-                                            primary={
-                                                <Typography
-                                                    className='typ-primary'
-                                                    sx={{ display: 'inline' }}
-                                                    component="span"
-                                                    color="text.primary"
-                                                    fontSize={18}
-                                                    fontWeight={600}
-                                                >
-                                                    {event.name}
-                                                </Typography>
-                                            }
-                                            secondary={
-                                                <Fragment>
+                                            <ListItemText
+                                                primary={
                                                     <Typography
-                                                        color="text.primary"
-                                                        //fontWeight={600}
-                                                    >
-                                                        Tipo de evento: <br />
-                                                    </Typography>
-                                                    <Typography
+                                                        className='typ-primary'
                                                         sx={{ display: 'inline' }}
                                                         component="span"
-                                                        variant="body2"
                                                         color="text.primary"
+                                                        fontSize={18}
+                                                        fontWeight={600}
                                                     >
-                                                        {
-                                                            "Data: " + dateEvent.toLocaleDateString() + " | " +
-                                                            "Hora: " + dateEvent.toLocaleTimeString()
-                                                        }
-                                                        <br />Local:
+                                                        {event.name}
                                                     </Typography>
-                                                </Fragment>
-                                            }
-                                        />
-                                    </ListItem>
-                                </Link>
-                            </Paper>
-                        )
-                    })
-                }
-            </List>
-        </Box>
+                                                }
+                                                secondary={
+                                                    <Fragment>
+                                                        <Typography
+                                                            color="text.primary"
+                                                        //fontWeight={600}
+                                                        >
+                                                            Tipo de evento: <br />
+                                                        </Typography>
+                                                        <Typography
+                                                            sx={{ display: 'inline' }}
+                                                            component="span"
+                                                            variant="body2"
+                                                            color="text.primary"
+                                                        >
+                                                            {
+                                                                "Data: " + dateEvent.toLocaleDateString() + " | " +
+                                                                "Hora: " + dateEvent.toLocaleTimeString()
+                                                            }
+                                                            <br />Local:
+                                                        </Typography>
+                                                    </Fragment>
+                                                }
+                                            />
+                                        </ListItem>
+                                    </Link>
+                                </Paper>
+                            )
+                        })
+                    }
+                </List>
+            </Box>
+        </Container>
     );
 }
